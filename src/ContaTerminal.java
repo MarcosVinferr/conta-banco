3a862ff3b296e2cb6fbdea77c75d15061f0cfeb8
Marcosvinferr <marvincomex@gmail.com>
2023-09-28 05:50:30 -0300

    projeto concluido

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,28 @@
+import java.text.DecimalFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("digite o numero da sua conta: ");
+        int numero = scanner.nextInt();
+
+        System.out.println("digite o numero da sua Agência: ");
+        String agencia = scanner.next();
+
+        System.out.println("digite o seu nome. ");
+        String nome = scanner.next();
+
+        System.out.println("digite o seu sobrenome. ");
+        String sobrenome = scanner.next();
+
+        System.out.println("digite o saldo da sua conta.");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Ola " + nome +" "+ sobrenome +", obrigado por criar uma conta em nosso banco, sua agência é " 
+                            + agencia +", conta " + numero + " e seu saldo é de R$ " 
+                            + saldo + " Já esta disponivel para saque. " );
     }
 }
